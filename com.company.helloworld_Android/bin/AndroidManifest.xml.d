66e3f58b8d45e12896249cff8f9fa21e8f18ead2
takes2 <takes2.soft@gmail.com>
2015-05-03 18:50:21 +0900

    gh-pages作成

--- com.company.helloworld_Android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\git\repos\HELLO_~1\COMCOM~1.HEL\bin\AndroidManifest.xml : \
-C:\git\repos\HELLO_~1\COMCOM~1.HEL\AndroidManifest.xml \
--- com.company.helloworld_Android/bin/helloworld-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\git\repos\HELLO_~1\COMCOM~1.HEL\bin\helloworld-debug-unaligned.apk : \
-C:\git\repos\HELLO_~1\COMCOM~1.HEL\bin\helloworld.ap_ \
-C:\git\repos\HELLO_~1\COMCOM~1.HEL\bin\classes.dex \
-C:\git\repos\HELLO_~1\COMCOM~1.HEL\libs\cordova.jar \
-C:\Users\takes2\AppData\Local\Android\sdk\tools\support\annotations.jar \
